d5b2b437a5b71c1576a86a020419300a16c30f04
Malikethx <252501@edu.p.lodz.pl>
2024-01-22 10:46:19 +0100

    nowa metoda policz

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int policz(int a, int b)
+{
+    return a + b;
+}
+
 int main() {
     printf("Hello, Worl d!\n");
+    printf("branch 01 %d", policz(2, 3));
     return 0;
 }
